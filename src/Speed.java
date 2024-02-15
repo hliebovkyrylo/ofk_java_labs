763cea387cebbd29219cae9a1193abea52e23256
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-02-15 11:20:50 +0200

    speed calculation

---
+++ src/Speed.java
@@ -0,0 +1,10 @@
+public class Speed {
+  public static void main(String[] args) {
+    double distance = 100.0;
+    double time = 9.58;
+
+    double speed = distance/time;
+
+    System.out.println("Швидкість: " + speed + "м/с");
+  }
+}
